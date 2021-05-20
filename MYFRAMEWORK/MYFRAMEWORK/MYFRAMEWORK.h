f3136217acb34d63b17553f8edba03dfdcc555d9
DarshanaSoni <84092429+DarshanaSoni@users.noreply.github.com>
2021-05-20 20:11:58 +0530

    Add files via upload

---
+++ MYFRAMEWORK/MYFRAMEWORK/MYFRAMEWORK.h
@@ -0,0 +1,18 @@
+//
+//  MYFRAMEWORK.h
+//  MYFRAMEWORK
+//
+//  Created by Darshana Soni on 20/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MYFRAMEWORK.
+FOUNDATION_EXPORT double MYFRAMEWORKVersionNumber;
+
+//! Project version string for MYFRAMEWORK.
+FOUNDATION_EXPORT const unsigned char MYFRAMEWORKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MYFRAMEWORK/PublicHeader.h>
+
+
